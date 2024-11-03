54ded3cdda6582cd8174f3119e1d153e6adb8685
Max <maksimman2907@gmail.com>
2024-11-03 16:27:29 +0300

    Файл библиотеки

---
+++ sbext.py
@@ -0,0 +1,88 @@
+# Creation date: 3.11.2024
+# Update date: 3.11.2024
+# About: the library was created for it is own needs. I always wanted a small 
+#        and simple library that would help me make the output text beautiful 
+#        and convenient.
+
+class color:
+    
+    white = '\033[0m' # WHITE
+    c00 = '\033[0m'
+    red = '\033[31m' # RED
+    c01 = '\033[31m'
+    orange = '\033[91m' # ORANGE
+    c02 = '\033[91m'
+    green = '\033[32m' # GREEN
+    c03 = '\033[32m'
+    light_green = '\033[92m' # LIGHT GREEN
+    c04 = '\033[92m'
+    yellow = '\033[33m' # YELLOW
+    c05 = '\033[33m'
+    light_yellow = '\033[93m' # LIGHT YELLOW
+    c06 = '\033[93m'
+    blue = '\033[34m' # BLUE
+    c07 = '\033[34m'
+    light_blue = '\033[94m' # LIGHT BLUE
+    c08 = '\033[94m'
+    violet = '\033[35m' # VIOLET
+    c09 = '\033[35m'
+    light_violet = '\033[95m' # LIGHT VIOLET
+    c10 = '\033[95m'
+    turquoise = '\033[36m' # TURQUOISE
+    c11 = '\033[36m'
+    light_turquoise = '\033[96m' # LIGHT TURQUOISE
+    c12 = '\033[96m'
+    grey = '\033[90m' # GREY
+    c13 = '\033[90m'
+    light_grey = '\033[37m' # LIGHT GREY
+    c14 = '\033[37m'
+
+class background:
+
+    white = '\033[7m' # WHITE
+    b00 = '\033[7m'
+    red = '\033[41m' # RED
+    b01 = '\033[41m'
+    orange = '\033[101m' # ORANGE
+    b02 = '\033[101m'
+    green = '\033[42m' # GREEN
+    b03 = '\033[42m'
+    light_green = '\033[102m' # LIGHT GREEN
+    b04 = '\033[102m'
+    yellow = '\033[43m' # YELLOW
+    b05 = '\033[43m'
+    light_yellow = '\033[103m' # LIGHT YELLOW
+    b06 = '\033[103m'
+    blue = '\033[44m' # BLUE
+    b07 = '\033[44m'
+    light_blue = '\033[104m' # LIGHT BLUE
+    b08 = '\033[104m'
+    violet = '\033[45m' # VIOLET
+    b09 = '\033[45m'
+    light_violet = '\033[105m' # LIGHT VIOLET
+    b10 = '\033[105m'
+    turquoise = '\033[46m' # TURQUOISE
+    b11 = '\033[46m'
+    light_turquoise = '\033[106m' # LIGHT TURQUOISE
+    b12 = '\033[106m'
+    grey = '\033[100m' # GREY
+    b13 = '\033[100m'
+    light_grey = '\033[47m' # LIGHT GREY
+    b14 = '\033[47m'
+
+class style:
+
+    italic_font = '\033[3m' # ITALIC FONT
+    s01 = '\033[3m'
+    bold_font = '\033[1m' #BOLD FONT
+    s02 = '\033[1m'
+    flashing_font = '\033[5m' # FLASHING FONT
+    s03 = '\033[5m'
+    top_line = '\033[53m' # TOP LINE
+    s04 = '\033[53m'
+    bottom_line = '\033[4m' # BOTTOM LINE
+    s05 = '\033[4m'
+    two_bottom_lines = '\033[21m' # TWO BOTTOM LINES
+    s06 = '\033[21m'
+    cross_out = '\033[9m' # CROSS OUT
+    s07 = '\033[9m'
